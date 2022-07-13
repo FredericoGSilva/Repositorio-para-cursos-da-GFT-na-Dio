0e29682d204d3d2685f85ec08a29895649366936
FredericoGSilva <frederico.dev@outlook.com>
2022-07-13 11:08:29 -0300

    poo

---
+++ Curso GFT Digital Innovation One/src/bootcamp/programação_orientada_a_objetos/classeCarro.java
@@ -0,0 +1,44 @@
+package bootcamp.programação_orientada_a_objetos;
+
+public class classeCarro {
+    private String cor;
+    private String modelo;
+    private int capacidadeTanque;
+
+//  A linguagem automaticamente cria um método construtor vazio.
+    public classeCarro() {
+
+    }
+
+    // método construtor --- Sobrecarga do método construtor
+    public classeCarro(String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    public int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+    public void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    // método do total para encher o tanque
+    public double valorTotal(double valorDoCombustivel) {
+        return capacidadeTanque * valorDoCombustivel;
+    }
+
+}
---
+++ Curso GFT Digital Innovation One/src/bootcamp/programação_orientada_a_objetos/objetoCarro.java
@@ -0,0 +1,26 @@
+package bootcamp.programação_orientada_a_objetos;
+
+public class objetoCarro {
+    public static void main(String[] args) {
+        
+        classeCarro carro1 = new classeCarro("preto", "chevrolet", 59); // construtor com atributos
+
+        System.out.println("Cor do carro 1: " + carro1.getCor());
+        System.out.println("Modelo do carro 1: " + carro1.getModelo());
+        System.out.println("Capacidade do tanque do carro 1: " + carro1.getCapacidadeTanque());
+        System.out.printf("Valor do combustível %.2f: ", carro1.valorTotal(6.39));
+
+        System.out.println("\n--------------------------------------");
+
+        classeCarro carro2 = new classeCarro(); // construtor sem atributos
+
+        carro2.setCor("vermelho");
+        carro2.setModelo("Ford");
+        carro2.setCapacidadeTanque(66);
+        
+        System.out.println("Cor do carro 2: " + carro2.getCor());
+        System.out.println("Modelo do carro 2: " + carro2.getModelo());
+        System.out.println("Capacidade do tanque do carro 2: " + carro2.getCapacidadeTanque());
+        System.out.printf("Valor do combustível %.2f: ", carro2.valorTotal(6.46));
+    }
+}
